6807448d8ee8123bf94f6175f847dd4985737eeb
Sagar kumar <109957797+sagarkumar2301@users.noreply.github.com>
2026-04-18 11:52:24 +0530

    first commit

---
+++ demo_project1/Program.cs
@@ -0,0 +1,14 @@
+﻿using demo_project1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student();
+
+        s1.Name = "Sagar";
+        s1.Age = 22;
+
+        s1.Display();
+    }
+}
---
+++ demo_project1/Student.cs
@@ -0,0 +1,25 @@
+﻿using demo_project1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo_project1
+{
+    public class Student
+    {
+        public string Name;
+        public int Age;
+
+        public void Display()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Age: " + Age);
+        }
+    }
+
+   
+}
+  
